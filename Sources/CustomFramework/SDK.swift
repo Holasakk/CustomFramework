7e8d6d1773b984d830c0958e0233601e563c0137
Stanislav Holas <standaholas@seznam.cz>
2022-08-03 14:27:17 +0200

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Stanislav Holas on 03.08.2022.
+//
+
+import Foundation
+
+public struct SDK {
+    static func doSomeWork() {
+        print("Doing some work ...")
+    }
+}
